7227f672eda00243c726327b8d099d860b8f2834
youssefchannaoui <youssefchannaoui05@gmail.com>
2026-07-30 17:17:30 +0800

    Update opus.js

--- src/opus.js
+++ src/opus.js
@@ -221,7 +221,7 @@ export async function schedulePost({ projectId, clipId, account, title, descript
       publishAt: new Date(publishAt).toISOString(),
     },
   });
-  return res?.data ?? res;
+  return res?.data?.scheduleId;
 }
 
 /** Push a clip out right now. */
